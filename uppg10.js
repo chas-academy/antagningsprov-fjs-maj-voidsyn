275c222b46277725a1eb1d407b878cc5dd1ff468
kassemfuad1997@gmail.com <kassemfuad1997@gmail.com>
2025-05-31 20:18:09 +0200

    klara uppgifter!

--- uppg10.js
+++ uppg10.js
@@ -1,27 +1,5 @@
 function uppg10(){
-    const numbers = [5, 12, 7, 10, 20, 9, 11];
 
-    const bigEven = [];
-    const smallOdd = [];
-    const other = [];
-
-    for (let i =0; i < numbers.length; i++) {
-      const num = numbers[i];
-
-      if (num > 10 && num % 2 === 0) {
-        bigEven.push(num);
-      } else if (num < 10 && num % 2 !==0) {
-        smallOdd.push(num);
-      } else {
-        other.push(num);
-      }
-    }
-
-    return {
-      bigEven,
-      smallOdd,
-      other
-    };
   
     // skapa tre arrayer - bigEven, smallOdd, other
     // loopa igenom numbers och placera talen i respektive array
--- uppg9.js
+++ uppg9.js
@@ -1,26 +1,7 @@
 
 
 function uppg9 (){
-    function sort(arr) {
-        for (let i =0; i < arr.length; i++) {
-            const tal = arr[i];
-            if (tal % 2 === 0) {
-                console.log(tal, "jämt");
-            } else {
-                console.log(tal, "udda");
-            }
-        }
-    }
 
-    sort([3,8,5,12,7]);
-    // skapa en funktion som heter sort och tar in en array av siffror som argument
-    // i funktionen du loopa igenom arrayen och kolla med en if-sats om  talet är jämt eller udda
-    // i if-sattsen skriv ut i konsolen resultatet
-    // resultatet ska loggas i formatet: talet och sen ordet "jämt" eller "udda"
-
-    // anropa funktionen och skicka med en array som argument
-
-  
 }
 
 module.exports = { uppg9 };
